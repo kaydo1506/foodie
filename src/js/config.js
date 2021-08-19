d02a1c66fb4c4fb1e05075281b9241879aa5588f
kaydo1506 <okedoifeoma@yahoo.com>
2021-08-19 10:51:34 +0100

    header edit

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+// export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '687c78d9-b871-43e8-a1a9-324890f0fc00';
