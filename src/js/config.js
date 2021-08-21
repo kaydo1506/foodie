06f7547671db5d6fec205998842e2560fa491118
kaydo1506 <okedoifeoma@yahoo.com>
2021-08-21 15:30:06 +0100

    created value variable

--- src/js/config.js
+++ src/js/config.js
@@ -4,6 +4,7 @@ export const KEY = '687c78d9-b871-43e8-a1a9-324890f0fc00';
 export const MODAL_CLOSE_SEC = 2.5;
 
 const mediaQuery = window.matchMedia('(max-width: 600px)');
+let value;
 if (mediaQuery.matches) {
   value = 5;
 } else {
